93630f069bc129f144000d7cccf53411fdef1604
Jake <jake@thousandeyes.com>
2020-12-09 09:08:25 -0500

    Add mock tooltip

--- public/js/cylinderScene.js
+++ public/js/cylinderScene.js
@@ -1,26 +1,38 @@
+
 export default function(group, _, __, graphData, el) {
     const { nodes, links } = graphData;
 
     const offsets = {
-        x: 50,
+        x: 0,
         y: -600,
-        z: 50,
+        z: 0,
     };
 
     const scale = .01;
     const nodeKeyToSphere = {};
     const selected = {};
+    const yScale = .8;
+
+    const colors = [
+        '#08A4BA',
+        '#33DAEC',
+        '#03475E',
+        '#A6D5B0'
+    ];
     nodes.forEach(({ key, x, y }) => {
-        const r = 200 + (Math.random() - 0.5) * 50;
+        const r = 300 + (Math.random() - 0.5) * 50;
         const pos = {}
-        pos.x = scale * (r * Math.cos(x * 2 * Math.PI / 1500 + (Math.PI/2)) + offsets.x);
-        pos.y = scale * (y + offsets.y);
-        pos.z = scale * (r * Math.sin(x * 2 * Math.PI / 1500 + (Math.PI/2)) + offsets.z);
+        pos.x = scale * (r * Math.cos(x * 2 * Math.PI / 1200 + (Math.PI/3)) + offsets.x);
+        pos.y = yScale * scale * (y + offsets.y);
+        pos.z = scale * (r * Math.sin(x * 2 * Math.PI / 1200 + (Math.PI/3)) + offsets.z);
 
         const sphereEl = document.createElement('a-sphere');
         sphereEl.setAttribute('position', `${pos.x} ${pos.y} ${pos.z}`)
-        sphereEl.setAttribute('radius', .03);
-        sphereEl.setAttribute('color', '#a082bf');
+
+        const radius = .03 + Math.random() * 0.03;
+        const color = pickRandom(colors);
+        sphereEl.setAttribute('radius', radius);
+        sphereEl.setAttribute('color', color);
         sphereEl.setAttribute('class', 'collidable');
 
         ['hoverable', 'grabbable', 'stretchable', 'draggable', 'droppable'].forEach(
@@ -38,20 +50,26 @@ export default function(group, _, __, graphData, el) {
 
         sphereEl.addEventListener('hover-end', function(event) {
             if (!selected[key]) { 
-                sphereEl.setAttribute('color', '#a082bf');
-                sphereEl.setAttribute('radius', .03);
+                sphereEl.setAttribute('color', color);
+                sphereEl.setAttribute('radius', radius);
             }
         });
 
         sphereEl.onclick = function(event) {
+            const tipElem = document.querySelector('#node-tip');
+
             if (selected[key]) {
-                sphereEl.setAttribute('color', '#a082bf');
-                sphereEl.setAttribute('radius', .03);
+                sphereEl.setAttribute('color', color);
+                sphereEl.setAttribute('radius', radius);
+                tipElem.setAttribute('visible', false);
             }
             else {
                 sphereEl.setAttribute('color', '#fca903');
                 sphereEl.setAttribute('radius', .07);
+                tipElem.setAttribute('visible', true);
+
                 selected[key] = true;
+
             }
         };
 
@@ -75,4 +93,9 @@ export default function(group, _, __, graphData, el) {
         group.add(line);
     });
 
+}
+
+
+function pickRandom(array) {
+    return array[Math.floor(Math.random() * array.length)];
 }
